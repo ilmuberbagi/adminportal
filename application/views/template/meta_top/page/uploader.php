577292c79c607e248bb417537e043d907ddc7dde
Puguh Jayadi <puguh.jayadi@gmail.com>
2016-04-21 14:19:40 +0700

    upload image - sql

--- application/views/template/meta_top/page/uploader.php
+++ application/views/template/meta_top/page/uploader.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="<?php echo base_url().'assets/plugins/jquery-fileupload/css/blueimp-gallery.min.css';?>">
 <link rel="stylesheet" href="<?php echo base_url().'assets/plugins/jquery-fileupload/jquery.fileupload.css';?>">
 <link rel="stylesheet" href="<?php echo base_url().'assets/plugins/jquery-fileupload/jquery.fileupload-ui.css';?>">
 <noscript><link rel="stylesheet" href="<?php echo base_url().'assets/plugins/jquery-fileupload/jquery.fileupload-noscript.css';?>"></noscript>
--- application/views/template/page/uploader.php
+++ application/views/template/page/uploader.php
@@ -20,28 +20,34 @@
 			</div>
 			<div class="box-body">
 				<!-- uploader -->
-				<form id="fileupload" action="<?php echo base_url().'asset/do_upload'?>" method="POST" enctype="multipart/form-data">
+				<form id="fileupload" action="<?php echo base_url().'asset/do_upload';?>" method="POST" enctype="multipart/form-data">
+					<!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
 					<div class="row fileupload-buttonbar">
-						<div class="col-lg-12">
+						<div class="col-lg-7">
+							<!-- The fileinput-button span is used to style the file input field as button -->
 							<span class="btn btn-success fileinput-button">
-								<i class="fa fa-plus"></i>
-								<span>Add Image files to upload</span>
-								<input type="file" id="userfile" name="userfile" multiple>
+								<i class="glyphicon glyphicon-plus"></i>
+								<span>Add files...</span>
+								<input type="file" name="userfile" multiple>
 							</span>
 							<button type="submit" class="btn btn-primary start">
-								<i class="fa fa-cloud-upload"></i>
+								<i class="glyphicon glyphicon-upload"></i>
 								<span>Start upload</span>
 							</button>
-							<button type="reset" class="btn btn-warning cancel cancel-all">
-								<i class="fa fa-ban"></i>
+							<button type="reset" class="btn btn-warning cancel">
+								<i class="glyphicon glyphicon-ban-circle"></i>
 								<span>Cancel upload</span>
 							</button>
+							<button type="button" class="btn btn-danger delete">
+								<i class="glyphicon glyphicon-trash"></i>
+								<span>Delete</span>
+							</button>
+							<input type="checkbox" class="toggle">
 							<!-- The global file processing state -->
 							<span class="fileupload-process"></span>
 						</div>
-						
 						<!-- The global progress state -->
-						<div class="col-lg-12 fileupload-progress fade">
+						<div class="col-lg-5 fileupload-progress fade">
 							<!-- The global progress bar -->
 							<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
 								<div class="progress-bar progress-bar-success" style="width:0%;"></div>
@@ -50,85 +56,97 @@
 							<div class="progress-extended">&nbsp;</div>
 						</div>
 					</div>
-					<!-- Drop zone -->
-					<div>  </div>
 					<!-- The table listing the files available for upload/download -->
-					<table role="presentation" class="table"><tbody class="files"></tbody></table>
+					<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
 				</form>
+				<!-- The blueimp Gallery widget -->
+				<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-filter=":even">
+					<div class="slides"></div>
+					<h3 class="title"></h3>
+					<a class="prev">‹</a>
+					<a class="next">›</a>
+					<a class="close">×</a>
+					<a class="play-pause"></a>
+					<ol class="indicator"></ol>
+				</div>
+
 			</div>
 		</div>
 	</section>
 </div>
 
-			<!-- The template to display files available for upload -->
-			<script id="template-upload" type="text/x-tmpl">
-			{% for (var i=0, file; file=o.files[i]; i++) { %}
-			    <tr class="template-upload fade">
-			        <td>
-			            <span class="preview"></span>
-			        </td>
-			        <td class="vam">
-			            <p class="name">{%=file.name%}</p>
-			            <strong class="error text-danger"></strong>
-			        </td>
-			        <td>
-			            <p class="size">Processing...</p>
-			            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-			        </td>
-			        <td class="vam">
-			            {% if (!i && !o.options.autoUpload) { %}
-			                <button class="btn btn-primary start" disabled>
-			                    <i class="glyphicon glyphicon-upload"></i>
-			                    <span>Start</span>
-			                </button>
-			            {% } %}
-			            {% if (!i) { %}
-			                <button class="btn btn-warning cancel cancel-one {%=file.lastModified%}">
-			                    <i class="glyphicon glyphicon-ban-circle"></i>
-			                    <span>Cancel</span>
-			                </button>
-			            {% } %}
-			        </td>
-			    </tr>
+<!-- The template to display files available for upload -->
+<script id="template-upload" type="text/x-tmpl">
+	{% for (var i=0, file; file=o.files[i]; i++) { %}
+	<tr class="template-upload fade">
+		<td>
+			<span class="preview"></span>
+		</td>
+		<td>
+			<p class="name">{%=file.name%}</p>
+			<strong class="error text-danger"></strong>
+		</td>
+		<td>
+			<p class="size">Processing...</p>
+			<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+		</td>
+		<td>
+			{% if (!i && !o.options.autoUpload) { %}
+			<button class="btn btn-primary start" disabled>
+				<i class="glyphicon glyphicon-upload"></i>
+				<span>Start</span>
+			</button>
 			{% } %}
-			</script>
-
-			<!-- The template to display files available for download -->
-			<script id="template-download" type="text/x-tmpl">
-			{% for (var i=0, file; file=o.files[i]; i++) { %}
-			    <tr class="template-download fade {%=file.refid%}">
-			        <td>
-			            <span class="preview">
-			                {% if (file.thumbnailUrl) { %}
-			                    <!--<a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>-->
-								<!--<video src="{%=file.url%}" controls=""></video>-->
-			                {% } %}
-			            </span>
-			        </td>
-			        <td class="vab">
-			            <p class="name">
-			                {% if (file.url) { %}
-			                    {%=file.name%}
-			                {% } else { %}
-			                    <span>{%=file.name%}</span>
-			                {% } %}
-			            </p>
-			        </td>
-			        <td class="vam">
-						{% if (file.error) { %}
-			                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-						{% } else { %}
-							<div><span class="label label-success"><i class="glyphicon glyphicon-ok"></i> Upload success</span></div>
-			            {% } %}
-			        </td>
-			        <td class="vam">
-						{% if (file.url) { %}
-							<a class="btn btn-inverse btn-edit-upload" href="#">
-			                    <i class="fa fa-edit"></i>
-			                    <span>Edit File</span>
-			                </a>
-						{% } %}
-			        </td>
-			    </tr>
+			{% if (!i) { %}
+			<button class="btn btn-warning cancel">
+				<i class="glyphicon glyphicon-ban-circle"></i>
+				<span>Cancel</span>
+			</button>
 			{% } %}
-			</script>
+		</td>
+	</tr>
+	{% } %}
+</script>
+<!-- The template to display files available for download -->
+<script id="template-download" type="text/x-tmpl">
+	{% for (var i=0, file; file=o.files[i]; i++) { %}
+	<tr class="template-download fade">
+		<td>
+			<span class="preview">
+				{% if (file.thumbnailUrl) { %}
+				<a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+				{% } %}
+			</span>
+		</td>
+		<td>
+			<p class="name">
+				{% if (file.url) { %}
+				<a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
+				{% } else { %}
+				<span>{%=file.name%}</span>
+				{% } %}
+			</p>
+			{% if (file.error) { %}
+			<div><span class="label label-danger">Error</span> {%=file.error%}</div>
+			{% } %}
+		</td>
+		<td>
+			<span class="size">{%=o.formatFileSize(file.size)%}</span>
+		</td>
+		<td>
+			{% if (file.deleteUrl) { %}
+			<button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+				<i class="glyphicon glyphicon-trash"></i>
+				<span>Delete</span>
+			</button>
+			<input type="checkbox" name="delete" value="1" class="toggle">
+			{% } else { %}
+			<button class="btn btn-warning cancel">
+				<i class="glyphicon glyphicon-ban-circle"></i>
+				<span>Cancel</span>
+			</button>
+			{% } %}
+		</td>
+	</tr>
+	{% } %}
+</script>
